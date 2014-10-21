7e6dcb94266f543eba644f2eca54bb4e61769e39
Vsevolod Stakhov <vsevolod@highsecure.ru>
2014-10-21 16:01:03 +0100

    Add xchacha20 stream cipher.

--- sodium/crypto_stream_chacha20.h
+++ sodium/crypto_stream_chacha20.h
@@ -28,6 +28,9 @@ size_t crypto_stream_chacha20_keybytes(void);
 SODIUM_EXPORT
 size_t crypto_stream_chacha20_noncebytes(void);
 
+#define crypto_stream_xchacha20_KEYBYTES 32U
+#define crypto_stream_xchacha20_NONCEBYTES 24U
+
 SODIUM_EXPORT
 int crypto_stream_chacha20(unsigned char *c, unsigned long long clen,
                            const unsigned char *n, const unsigned char *k);
@@ -42,6 +45,10 @@ int crypto_stream_chacha20_xor_ic(unsigned char *c, const unsigned char *m,
                                   unsigned long long mlen,
                                   const unsigned char *n, uint64_t ic,
                                   const unsigned char *k);
+
+SODIUM_EXPORT
+int crypto_stream_xchacha20(unsigned char *c,unsigned long long clen,
+        const unsigned char *n, const unsigned char *k);
 #ifdef __cplusplus
 }
 #endif
